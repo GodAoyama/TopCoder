411981ded965d70901d5068ea6407a2cbd8a5098
GodAoyama <maekuma@gmail.com>
2014-04-25 16:23:47 +0900

    間違えた

--- Div2_618/easy/miyamoto.cs
+++ Div2_618/easy/miyamoto.cs
@@ -1,68 +1,17 @@
 using System;
 using System.Linq;
-using System.Collections;
 
-class LongWordsDiv2
+class
+WritingWords
 {
-    public string find(string word)
+    public int write(string word)
     {
-        bool ans = true;
-        string a = "";
-        string b = "";
-
-
-        for (int i = 0; i < word.Length - 1; i++)
+        int ans = 0;
+        System.Console.WriteLine((int)'A');
+        for (int i = 0; i < word.Length; i++)
         {
-            if (word[i] == word[i + 1])
-            {
-                return "Dislikes";
-            }
+            ans += word[i] - 64;
         }
-
-
-        for (int i = 0; i < word.Length - 1; i++)
-        {
-            for (int j = i + 1; j < word.Length; j++)
-            {
-                //System.Console.WriteLine("i = " + i + " j = " + j);
-                if (word[i] == word[j])
-                {
-                    a = word.Substring(i + 1, j - i);
-                    b = word.Substring(j + 1, word.Length - j + 1);
-
-                }
-
-                foreach (char c1 in a)
-                {
-                    foreach (char c2 in b)
-                    {
-                        if (c1 == c2)
-                        {
-                            return "Dislikes";
-                        }
-                    }
-                }
-
-
-                a = "";
-                b = "";
-
-
-            }
-
-            
-        }
-
-
-            if (ans == true)
-            {
-                return "Likes";
-            }
-            else
-            {
-                return "Dislikes";
-            }
+            return ans;
     }
-    
 }
-
