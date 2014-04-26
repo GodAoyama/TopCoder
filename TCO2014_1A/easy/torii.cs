93f1833dba9a40125125682b2aef88c1a6086b4c
torii <countryroad2317@yahoo.co.jp>
2014-04-27 00:00:03 +0900

    debug文削除, 処理の一元化

--- TCO2014_1A/easy/torii.cs
+++ TCO2014_1A/easy/torii.cs
@@ -6,17 +6,12 @@ public class EllysSortingTrimmer
 {
     public string getMin(string S, int L)
     {
-        System.Diagnostics.Debug.WriteLine(S[0]);
-        for (int i = S.Length-L; i > 0; i--)
+        for (int i = S.Length-L; i >= 0; i--)
         {
             char[] tmpStr = S.Substring(i,L).ToCharArray();
             Array.Sort(tmpStr);
             S = S.Substring(0, i) + new string(tmpStr);
         }
-
-
-        char[] answer =S.Substring(0, L).ToCharArray();
-        Array.Sort(answer);
-        return new string(answer);
+        return S;
     }
 }
